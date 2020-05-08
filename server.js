bd88ad296f917602862477c133b2dc7a5f05f430
belal <belal20795@gmail.com>
2020-05-08 07:34:02 +0530

    added img src with the api png

--- server.js
+++ server.js
@@ -16,15 +16,22 @@ app.get("/", (req, res) => {
             const weatherData = JSON.parse(data);
             const temp = weatherData.list[0].main.temp;
             const description = weatherData.list[0].weather[0].description;
+            const icon = weatherData.list[0].weather[0].icon
+            const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`
             // const temp = weatherData[0].main.temp
             // const weatherData2 = JSON.stringify(weatherData);
             // console.log(weatherData2);
             // const weatherData3 = JSON.parse(weatherData2);
             // console.log(weatherData3);
+            const city = weatherData.city.name
             console.log(temp, description);
+            res.write(`<h1>the temperatur in ${city} is </h1> `)
+            res.write(`<h1> ${temp} degree celcius and its describe as ${description}</h1>`)
+            res.write(`<img src="${iconUrl}" alt="icon">`);
+            res .send()
         });
     })
-    res .send("its working")
+   
 
 
 });
