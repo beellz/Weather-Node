2e8b978859680effbfd049d2e2c9b464ec36f5a7
belal <belal20795@gmail.com>
2020-05-09 20:40:59 +0530

    adding comments to understand better'

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Weather-Node
+
+This is a simple node wether application to check the weather from the openweather map .
+you can view this website here :
--- server.js
+++ server.js
@@ -7,37 +7,34 @@ const bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
-
-   
+    // this will send index.html when request of get comes .
+    
 
     res.sendFile(__dirname + "/index.html")
      
-
 });
 
 app.post("/" , (req, res ) => {
+
+    // this the url 
     const url ="https://api.openweathermap.org/data/2.5/forecast?q="
+    // this is to get the response from the body
     const city = req.body.cityName
     const appid = "&appid=cdefebdefc2001cd562f7af991ab145e&"
     const units = "units=metric"
     const urlAll = url + city + appid + units;
     
-
+// this url ends here .
 
     https.get(urlAll , (response) => {
         console.log(response.statusCode);
-
+// this is the response request 
         response.on("data",(data) => {
             const weatherData = JSON.parse(data);
             const temp = weatherData.list[0].main.temp;
             const description = weatherData.list[0].weather[0].description;
             const icon = weatherData.list[0].weather[0].icon
             const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`
-            // const temp = weatherData[0].main.temp
-            // const weatherData2 = JSON.stringify(weatherData);
-            // console.log(weatherData2);
-            // const weatherData3 = JSON.parse(weatherData2);
-            // console.log(weatherData3);
             const city = weatherData.city.name
             console.log(temp, description);
             res.write(`<div class="root"> `)
@@ -48,11 +45,8 @@ app.post("/" , (req, res ) => {
             res .send()
         });
     })
-
-
-    
 })
 
 
-
+// this is app listen to the port 
  app.listen(3000, () => { console.log("app is on port 3000")});
